5c3282db8a9e88af614769004865645c17272fbd
Samuel <130785902+Sammy-Am-I@users.noreply.github.com>
2023-11-29 20:02:13 -0500

    Update server.js

--- server.js
+++ server.js
@@ -13,49 +13,35 @@ const characters = [
         image: 'images/Mario.jpg',
         //Implement boolean to track for if character is set as favorite
         isFavorite: false,
-        summary:'First Appearance: 1983
-        Franchise: Mario
-        A famous plumber from the Mushroom Kingdom.
-        He is known for saving Princess Peach from the evil lord Bowser.'
+        summary:'First Appearance: 1983 \\n Franchise: Mario \\n A famous plumber from the Mushroom Kingdom. \\n He is known for saving Princess Peach from the evil lord Bowser.'
     },
     {
         name:'Pikachu',
         description: 'Electric Mouse Pokemon',
         image: 'images/Pikachu.jpg',
         isFavorite: false,
-        summary: 'First Appearance: 1996
-        Franchise: Pokemon
-        The electric mouse Pokemon and the mascot of the Pokemon series.
-        Know for their powerful electric moves like Thunder Jolt and Thunder.'
+        summary: 'First Appearance: 1996 \\n Franchise: Pokemon \\n The electric mouse Pokemon and the mascot of the Pokemon series. \\n Know for their powerful electric moves like Thunder Jolt and Thunder.'
     },
     {
         name: 'Fox',
         description: 'Superstar Pilot',
         image: 'images/Fox.jpg',
         isFavorite: false,
-        summary: 'First Appearance: 1993
-        Franchise: StarFox
-        Fox McCloud is the leader of the space mercenaries StarFox.
-        He is a young ace pilot with a strong sense of justice.' 
+        summary: 'First Appearance: 1993 \\n Franchise: StarFox \\n Fox McCloud is the leader of the space mercenaries StarFox. \\n He is a young ace pilot with a strong sense of justice.' 
     },
     {
         name: 'Samus',
         description: 'Intergalactic Bounty Hunter',
         image: 'images/Samus.jpg',
         isFavorite: false,
-        summary: 'First Appearance: 1986
-        Franchise: Metroid
-        Samus Aran is a famous galactic bounty hunter feared across the galaxy.
-        She battles countless foes ranging from Space pirates to the dreaded Metroid.'  
+        summary: 'First Appearance: 1986 \\n Franchise: Metroid \\n Samus Aran is a famous galactic bounty hunter feared across the galaxy. \\n She battles countless foes ranging from Space pirates to the dreaded Metroid.'  
     },
     {
         name: 'Captain Falcon',
         description: 'Part Time Bounty Hunter/Racer',
         image: 'images/Captain_Falcon.jpg',
         isFavorite: false,
-        summary: 'First Appearance: 1990
-        Franchise: F-Zero
-        A mysterious person known as the best racer in F-Zero and a renowned bounty hunter.' 
+        summary: 'First Appearance: 1990 \\n Franchise: F-Zero \\n A mysterious person known as the best racer in F-Zero and a renowned bounty hunter.' 
     },
 ];
 
